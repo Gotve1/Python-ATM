3d1bd0d0a43a8cefeaf6dbb04f79d73e01a7f38e
Gotve <akselvolkov40@gmail.com>
2026-01-29 09:01:00 +0500

    Initial Commit

---
+++ Main.py
@@ -0,0 +1,13 @@
+def main():
+    print(
+        "Please choose one of available options \n"
+        "1. Register a new credit card \n"
+        "2. Withdraw money \n"
+        "3. Input money \n"
+        "4. View my cards \n"
+        "5. Delete a card"
+    )
+
+
+if __name__ == "__main__":
+    main()
