472da0af3598a3720e75a4cf2dad2b55790305c1
Gotve <akselvolkov40@gmail.com>
2026-01-31 07:31:47 +0500

    Added register_a_new_credit_card() function, Used CreditCard and CreditCardProvider

--- dev/gotve/python-atm/Main.py
+++ dev/gotve/python-atm/Main.py
@@ -1,7 +1,8 @@
 import sys
 
 from Person import Person
-from CreditCard import CreditCard
+from CreditCard import CreditCard, CreditCardProvider
+
 
 def main():
     print(
@@ -19,7 +20,7 @@ def main():
 def user_input_handler():
     match input():
         case '1':
-            print("you choosed option 1")
+            register_a_new_credit_card()
         case '2':
             print("you choosed option 2")
         case '3':
@@ -30,8 +31,44 @@ def user_input_handler():
             print("you choosed option 5")
         case '6':
             sys.exit()
-        case _:
+        case _:  # used as "default" block in java
             print("Invalid option")
+
+
+def register_a_new_credit_card():
+    person = Person()
+    print("Enter your first name")
+    person.set_firstname(input())
+
+    print("Enter your last name")
+    person.set_lastname(input())
+
+    try:
+        print("Enter your age")
+        person_age = input()
+        person.set_age(int(person_age))
+    except ValueError:
+        print("You cannot input letters in your age, please try again. \n")
+
+    print(
+        "Choose one of card providers \n"
+        "1. Kapital Bank \n"
+        "2. NBU Bank \n"
+        "3. Infin Bank \n"
+        "4, TBC Bank \n"
+    )
+    credit_card = CreditCard()
+    match input():
+        case '1':
+            credit_card.set_credit_card_provider(CreditCardProvider.KAPITAL_BANK)
+        case '2':
+            credit_card.set_credit_card_provider(CreditCardProvider.NATIONAL_BANK_OF_UZBEKISTAN)
+        case '3':
+            credit_card.set_credit_card_provider(CreditCardProvider.INFIN_BANK)
+        case '4':
+            credit_card.set_credit_card_provider(CreditCardProvider.TBC_BANK)
+        case _:
+            print("Invalid bank provider")
 
 
 if __name__ == "__main__":
